6a6f8d47f04da076b538b97aa5e8af035850c6b4
tmachine <tmachine@sys04.PEC.IN>
2024-02-06 10:00:57 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,53 +1,65 @@
-import { useState } from 'react';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
+import  Task  from "./Task";
 
 function App() {
-  const[todoList,setTodoList]=useState([]);
-  const[newTask,setNewTask]=useState("");
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
 
-  const handleChange=(event)=>{
+  const handleChange = (event) => {
     setNewTask(event.target.value);
   };
-  
-  const addTask=()=>{
-    const task={
-      id: todoList.length === 0 ? 1 : todoList[todoList.length-1].id + 1,
-      taskName: newTask
-    };
-    setTodoList([...todoList,task]);
-  }
 
-  const deleteTask=(id)=>{
-    
-    setTodoList(todoList.filter((task)=>task.id!==id));
-  }
+  const addTask = () => {
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+      completed: false,
+    };
+    setTodoList(task.taskName !== "" ? [...todoList, task] : todoList);
+  };
+
+  const deleteTask = (id) => {
+    setTodoList(todoList.filter((task) => task.id !== id));
+  };
+
+  const completeTask = (id) => {
+    setTodoList(
+      todoList.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: true };
+        } else {
+          return task;
+        }
+      })
+    );
+  };
 
   return (
     <div className="App">
-      <p><b>TODO LIST</b></p>
       <div className="addTask">
-        
-        
-        <input onChange={handleChange}/>
-        <button className="hi" onClick={addTask}>Add Task</button>
+        <input onChange={handleChange} />
+        <button onClick={addTask}> Add Task</button>
       </div>
       <div className="list">
-        {todoList.map((task=>{
+        {todoList.map((task) => {
           return (
-          <div>
-            <h1>{task.taskName}</h1>
-            <button style={{alignItems:"center"}}   onClick={()=> deleteTask(task.id)}>x</button>
-          </div>);
-        }))}
+            <Task
+              taskName={task.taskName}
+              id={task.id}
+              completed={task.completed}
+              deleteTask={deleteTask}
+              completeTask={completeTask}
+            />
+          );
+        })}
       </div>
-      
     </div>
   );
 }
 
-
-
-
-
 export default App;
+
+
+
+
